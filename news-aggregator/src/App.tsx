c94136e75a9ffee87eacc3d952869c389d0e135f
Ivan-Synchuk <ivan.synchuk.ri.2024@lpnu.ua>
2026-02-10 14:56:47 +0200

    feat: initialize news aggregator project with React and Vite

---
+++ news-aggregator/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        News Aggregator Setup
+      </h1>
+    </div>
+  );
+}
+
+export default App;
