ee66cbc5e803ae5596272c9761935fb9d4b1397d
MrJ0nik <sun4uk1234@gmail.com>
2026-02-10 16:14:05 +0200

    feat: add axios dependency and create API clients for news and CMS

---
+++ news-aggregator/src/lib/api-clients.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const newsApiClient = axios.create({
+  baseURL: "https://newsapi.org/v2",
+
+  headers: {
+    "X-Api-Key": import.meta.env.VITE_NEWS_API_KEY,
+  },
+});
+
+export const cmsClient = axios.create({
+  baseURL: `https://${import.meta.env.VITE_SANITY_PROJECT_ID}.api.sanity.io/v2022-03-07/data/query/production`,
+});
